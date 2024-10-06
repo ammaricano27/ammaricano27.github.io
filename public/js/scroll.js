72bb7de9b60500f717c9257dae96118deb94a715
Marr <ammarabdul27@gmail.com>
2024-10-06 22:08:34 +0700

    fix header navbar darkmode

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -4,13 +4,13 @@ function onScroll() {
         header.classList.add("backdrop-blur-sm")
         header.classList.add("bg-white/[0.8]")
         //  header.classList.add("bg-opacity-50")
-        header.classList.add("dark:bg-black")
+        header.classList.add("dark:bg-black/[0.3]")
         header.classList.add("shadow-sm")
     } else {
         header.classList.remove("backdrop-blur-sm")
         header.classList.remove("bg-white/[0.8]")
         // header.classList.remove("bg-opacity-50")
-        header.classList.remove("dark:bg-black")
+        header.classList.remove("dark:bg-black/[0.3]")
         header.classList.remove("shadow-sm")
     }
 }
